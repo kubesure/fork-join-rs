21684570564bc35a562bd75b20c3cdea62a9cf79
kubesure <edakghar@gmail.com>
2023-08-16 23:14:51 +0400

    BB 3 - implmented worker with concurrent forkjoin

--- forkjoin/src/lib.rs
+++ forkjoin/src/lib.rs
@@ -1,36 +1,50 @@
 use anyhow::Result;
-
-use crossbeam::channel::{unbounded, Receiver, Sender};
+use crossbeam::{
+    channel::{after, unbounded, Receiver, Sender},
+    select,
+};
 use crossbeam_utils::sync::WaitGroup;
 use log::{error, info};
+use std::time::Duration;
+use std::{any::Any, sync::Arc};
 use thiserror::Error;
 
-#[derive(Debug)]
-struct FJResult<'a> {
+/*#[derive(Debug)]
+struct FJResult<D: Data> {
     id: String,
-    //x: Box<dyn Any>,
-    err: &'a FJError<'a>,
+    data: Arc<D>,
+    err: FJError,
+}*/
+
+#[derive(Debug)]
+struct FJResult {
+    id: String,
+    data: Arc<Box<dyn Any + Send + Sync>>,
+    err: FJError,
 }
+
+trait Data: Send + Sync {}
 
 #[derive(Debug, Error)]
 #[non_exhaustive]
-enum FJError<'a> {
+enum FJError {
     #[error("error code: {0} Internal server error")]
-    InternalServer(&'a str),
+    InternalServer(String),
     #[error("error code: {0} Request error")]
-    RequestError(&'a str),
+    RequestError(String),
     #[error("error code: {0} Response error")]
-    ResponseError(&'a str),
+    ResponseError(String),
     #[error("error code: {0} Connection error")]
-    ConnectionError(&'a str),
+    ConnectionError(String),
     #[error("error code: {0} Concurrency context error")]
-    ConcurrencyContextError(&'a str),
+    ConcurrencyContextError(String),
 }
 
 struct Multiplexer<W: Worker> {
     workers: Vec<W>,
 }
 
+#[derive(Debug)]
 struct Input<W: Worker> {
     id: u32,
     wg: WaitGroup,
@@ -38,15 +52,15 @@ struct Input<W: Worker> {
 }
 
 trait Worker: Send {
-    fn work(&self) -> Result<Receiver<FJResult>, Receiver<Heartbeat>>;
+    fn work(&self) -> Result<(Receiver<FJResult>, Receiver<Heartbeat>)>;
     fn active_dead_line_seconds(&self) -> Result<u32>;
 }
-
+#[derive(Debug)]
 struct Heartbeat {
     id: u16,
 }
 
-const INTERNAL_SERVER_ERROR_CODE: &str = "001";
+//TODO const INTERNAL_SERVER_ERROR_CODE: String = String::from("001");
 
 impl<W: Worker> Multiplexer<W> {
     pub fn new() -> Self {
@@ -59,10 +73,10 @@ impl<W: Worker> Multiplexer<W> {
         self.workers.push(worker);
     }
 
-    pub fn multiplex<'a>(self) -> Result<Receiver<FJResult<'a>>, FJError<'a>> {
+    pub fn multiplex(self) -> Result<Receiver<FJResult>, FJError> {
         if self.workers.len() == 0 {
             error!("no workers added");
-            return Err(FJError::InternalServer(INTERNAL_SERVER_ERROR_CODE));
+            return Err(FJError::InternalServer("001".to_string()));
         }
 
         let (tx, rx) = unbounded();
@@ -76,11 +90,11 @@ impl<W: Worker> Multiplexer<W> {
                     wg: wg,
                     worker: wrkr,
                 };
-                let tx = tx.clone();
+                let mutiplex_result_stream = tx.clone();
+
                 let handle = s.spawn(move |_| {
-                    manage(input, tx);
+                    manage(input, mutiplex_result_stream);
                 });
-                handle.join().unwrap();
             }
             wg.wait();
         });
@@ -89,20 +103,87 @@ impl<W: Worker> Multiplexer<W> {
     }
 }
 
-fn manage<W: Worker>(input: Input<W>, tx: Sender<FJResult>) {
-    input.worker.work();
+fn manage<W: Worker>(input: Input<W>, multiplex_result_stream: Sender<FJResult>) {
+    let wg: WaitGroup = input.wg;
+    let result: Result<(Receiver<FJResult>, Receiver<Heartbeat>)> =
+        dispatch(input.worker, Duration::from_secs(1));
+
+    match result {
+        Ok(results) => loop {
+            select! {
+                recv(results.1) -> beat => {
+                    if let Ok(beat) = beat {
+                        info!("received {:?}",beat.id);
+                        info!("received  beat {:?}", beat);
+                        return
+                    }
+                },
+                recv(results.0) -> result => {
+                    if let Ok(result) = result {
+                        info!("{:?}",result);
+                        multiplex_result_stream.send(result);
+                        drop(wg);
+                        return
+                    }
+                },
+            }
+        },
+        Err(_) => {
+            todo!()
+        }
+    }
 }
 
-struct TestWorker {}
+fn dispatch<W: Worker>(
+    worker: W,
+    pulse_interval: Duration,
+) -> Result<(Receiver<FJResult>, Receiver<Heartbeat>)> {
+    let worker_streams: Result<(Receiver<FJResult>, Receiver<Heartbeat>)> = worker.work();
+    worker_streams
+}
+
+fn send_pulse(sender: &Sender<Heartbeat>, pulse_interval_sec: u64) {
+    let interval = Duration::from_secs(pulse_interval_sec);
+
+    select! {
+        recv(after(interval)) -> _ => {
+            _ = sender.send(Heartbeat { id: 0 });
+        }
+    }
+}
+
+struct TestWorker {
+    active_deadline_seconds: u32,
+}
 impl Worker for TestWorker {
-    fn work(&self) -> Result<Receiver<FJResult>, Receiver<Heartbeat>> {
+    fn work(&self) -> Result<(Receiver<FJResult>, Receiver<Heartbeat>)> {
         info!("I am Testworker");
-        todo!()
+        let (result_tx, result_rx) = unbounded::<FJResult>();
+        let (heat_beat_tx, heart_beat_rx) = unbounded::<Heartbeat>();
+
+        let result = crossbeam::scope(|s| {
+            let handle = s.spawn(|_| {
+                send_pulse(&heat_beat_tx, 1);
+            });
+        });
+
+        let result = crossbeam::scope(|s| {
+            let self_clone = self.clone();
+            let handle = s.spawn(move |sc| {
+                self_clone.do_work(heat_beat_tx.clone());
+            });
+        });
+
+        Ok((result_rx, heart_beat_rx))
     }
 
     fn active_dead_line_seconds(&self) -> Result<u32> {
-        Ok(10)
+        Ok(self.active_deadline_seconds)
     }
+}
+
+impl TestWorker {
+    fn do_work(&self, heart_beat_tx: Sender<Heartbeat>) {}
 }
 
 #[cfg(test)]
@@ -112,8 +193,27 @@ mod tests {
     #[test]
     fn test_add_worker() {
         let mut multiplexer: Multiplexer<TestWorker> = Multiplexer::new();
-        multiplexer.add_worker(TestWorker {});
+        multiplexer.add_worker(TestWorker {
+            active_deadline_seconds: 10,
+        });
         assert_eq!(multiplexer.workers.len(), 1);
-        multiplexer.multiplex();
+        let result = multiplexer.multiplex();
+
+        match result {
+            Ok(result_stream) => loop {
+                match result_stream.recv() {
+                    Ok(value) => {
+                        info!("Received: {}", value.id);
+                    }
+                    Err(_) => {
+                        error!("all senders are dropped");
+                        break;
+                    }
+                }
+            },
+            Err(err) => {
+                error!("{}", err);
+            }
+        }
     }
 }
